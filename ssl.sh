ed2af5aca42a3caa2a792c75bbd6c66759d8570a
Administrator <admin@example.com>
2021-11-02 11:27:15 -0400

    update

--- ssl.sh
+++ ssl.sh
@@ -11,5 +11,4 @@ email=Hosting@rvadv.com
 openssl req -nodes -newkey rsa:2048 -keyout /etc/ssl/private/private.key -out /etc/ssl/private/request.csr \
 -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
 
-openssl x509 -in /etc/ssl/private/request.csr -out /etc/ssl/private/certificate.crt -req -signkey 
-/etc/ssl/private/private.key -days 365
+openssl x509 -in /etc/ssl/private/request.csr -out /etc/ssl/private/certificate.crt -req -signkey /etc/ssl/private/private.key -days 365
